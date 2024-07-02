a451b6c42febb5ea33a5655fc1b53ca46dd7cad9
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-02 13:58:05 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -3,14 +3,15 @@
 #include<string>
 
 char getSoundexCode(char c) {
-    c = std::toupper(c);
-    if (c=="B"||c=="F"||c=="P"||c=="V") return "1";
-    if (c=="C"||c=="G"||c=="J"||c=="K"||c=="Q"||c=="S"||c=="X"||c=="Z") return "2";
-    if (c=="D"||c=="T") return "3";
-    if (c=="L") return "4";
-    if (c=="M"||c=="N") return "5";
-    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'H' || c == 'W' || c == 'Y') return '0';
-    return '0';}
+   c = std::toupper(c);
+    if (c == 'C' || c == 'G' || c == 'J' || c == 'K' || c == 'Q' || c == 'S' || c == 'X' || c == 'Z') return '2';
+    if (c == 'B' || c == 'F' || c == 'P' || c == 'V') return '1';
+    if (c == 'D' || c == 'T') return '3';
+    if (c == 'L') return '4';
+    if (c == 'M' || c == 'N') return '5';
+    if (c == 'R') return '6';
+    return '0'; 
+}
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
