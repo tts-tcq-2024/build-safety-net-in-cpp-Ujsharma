06a9fadf2f645b910d9d645eff94839a1685f96b
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-02 15:54:48 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -19,17 +19,15 @@ std::string generateSoundex(const std::string& name) {
     std::string soundex(1, toupper(name[0]));
     char prevCode = getSoundexCode(name[0]);
 
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
+    for (size_t i = 1; i < name.length(); ++i) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
-        }
-    }
-
-    if (soundex.length() < 4) {
+       if (soundex.length() < 4){
+          if (code != '0' && code != prevCode) {
+             soundex += code;
+             prevCode = code;}
+       }
+       if (soundex.length() < 4) {
         soundex += '0';
     }
-
     return soundex;
 }
