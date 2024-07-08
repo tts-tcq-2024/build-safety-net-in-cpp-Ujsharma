21521b9aaadcff2d412e709cc44ff8acc441f2ad
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-08 10:45:15 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -2,7 +2,14 @@
 #define SOUNDEX_H
 
 #include <string>
+#include <unordered_map>
 
+extern const std::unordered_map<char, char> soundexMap;
+
+char getSoundexCode(char c);
+bool SoundexLengthCheck(const std::string& soundex);
+bool SoundexCodeCheck(char code, char prevCode);
 std::string generateSoundex(const std::string& name);
+std::string padSoundex(const std::string& soundex);
 
 #endif // SOUNDEX_H
