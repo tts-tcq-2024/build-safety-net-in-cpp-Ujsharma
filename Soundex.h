dd717a5026890aed1a9a59917fe87e233acb673f
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-08 10:16:12 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,6 +3,10 @@
 
 #include <string>
 
+char getSoundexCode(char c);
+bool SoundexLengthCheck(const std::string& soundex);
+bool SoundexCodeCheck(char code, char prevCode);
 std::string generateSoundex(const std::string& name);
+std::string padSoundex(const std::string& soundex);
 
 #endif // SOUNDEX_H
